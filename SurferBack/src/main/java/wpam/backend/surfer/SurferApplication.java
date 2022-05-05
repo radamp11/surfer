928b5fb0c8e1e9918626a78036087ec2ec7dec95
radamp11 <adamstec11@gmail.com>
2022-05-05 22:46:37 +0200

    surfer init

---
+++ SurferBack/src/main/java/wpam/backend/surfer/SurferApplication.java
@@ -0,0 +1,13 @@
+package wpam.backend.surfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurferApplication.class, args);
+    }
+
+}
---
+++ SurferBack/src/test/java/wpam/backend/surfer/SurferApplicationTests.java
@@ -0,0 +1,13 @@
+package wpam.backend.surfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
